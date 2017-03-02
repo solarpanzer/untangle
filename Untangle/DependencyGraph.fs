9ff07d143c755ccbd0d00fbf947d10917b0b5258
Sebastian Seifert <sebastian.dev@kontextfrei.de>
2017-03-02 06:24:43 +0100

    Move over sources from playground project.

---
+++ Untangle/DependencyGraph.fs
@@ -0,0 +1,88 @@
+﻿module DependencyGraph
+
+open QuickGraph
+open QuickGraph.Graphviz
+
+type Node = string
+type Edge = Node * Node
+type Graph = {nodes:Set<Node>; edges:Set<Edge>}
+
+module Qg = 
+    type QgNode = string
+    type QgEdge = SEdge<string>
+    type QgGraph = AdjacencyGraph<QgNode, QgEdge>
+        
+    let transitiveHullForNode (graph:QgGraph) (node:QgNode) =
+        let mutable hull = []
+        let dfs = Algorithms.Search.DepthFirstSearchAlgorithm(graph)
+        dfs.add_DiscoverVertex(fun v -> hull <- v :: hull)
+        dfs.SetRootVertex(node)
+        dfs.Compute()
+        hull |> Set.ofSeq
+
+    let graphToDot graph = 
+        let formatVertex (e : FormatVertexEventArgs<Node>) = 
+            e.VertexFormatter.Label <- e.Vertex
+        let graphvizAlgorithm = GraphvizAlgorithm<QgNode, QgEdge>(graph)
+        graphvizAlgorithm.FormatVertex.Add(formatVertex)
+        graphvizAlgorithm.Generate()
+
+    let toQgGraph g = 
+        let wrappedEdges = g.edges |> Seq.map (fun (a,b) -> QgEdge(a,b))
+        wrappedEdges.ToAdjacencyGraph()
+
+let fromNodesAndEdges nodeObjects edgeTuples = 
+    { nodes = Set.ofSeq nodeObjects; edges = Set.ofSeq edgeTuples }
+    
+let filterNodes predicate g = 
+    let newNodes = g.nodes |> Set.filter predicate
+    let newEdges = g.edges |> Set.filter (fun (a,b) -> (predicate a) && (predicate b))
+    fromNodesAndEdges newNodes newEdges
+
+let mapNodesMergeEdges graph nodeMapping =
+    let newNodeSet = graph.nodes |> Seq.map nodeMapping |> Set.ofSeq
+    let mapEdge (s, t) = (nodeMapping s, nodeMapping t)
+    let selfDependency (a,b) = (b = a)
+    let newEdgeSet = graph.edges |> Seq.map mapEdge |> Seq.filter (not << selfDependency) |> Set.ofSeq
+    fromNodesAndEdges newNodeSet newEdgeSet
+
+let graphToDot graph = graph |> Qg.toQgGraph |> Qg.graphToDot
+
+let graphToCsv graph =
+    let boolToSymbol b = if b then "X" else ""
+    let lineForSource source = 
+        seq {
+            yield source
+            yield! graph.nodes |> Seq.map (fun target -> Set.contains (source, target) graph.edges |> boolToSymbol)
+        } |> String.concat "," 
+    seq {
+        yield String.concat "," ("" :: (graph.nodes |> List.ofSeq))
+        yield! graph.nodes |> Seq.map lineForSource
+    } |> String.concat "\n"
+
+let csvToGraph (csvString:string) =
+    let lines = csvString.Split('\n')
+    let nodeNames = lines.[0] |> fun s -> s.Split(',') |> Seq.skip 1 |> Array.ofSeq
+    let edgeLineToEdgePairs (line:string) =
+        match line.Split(',') |> List.ofArray with
+            | source :: entries ->
+                entries 
+                |> List.mapi (fun i v -> (i, v))
+                |> List.filter (fun (i,v) -> v.Length > 0)
+                |> List.map (fun (i,v) -> (source, nodeNames.[i]))
+            | _ -> []   
+    let edges = 
+        let edgeLines = lines.[1..]
+        edgeLines |> Seq.collect edgeLineToEdgePairs
+    fromNodesAndEdges nodeNames edges
+
+let transitiveHullForNodeSet graph nodes = 
+    let qg = Qg.toQgGraph graph
+    nodes |> Seq.collect (Qg.transitiveHullForNode qg) |> Set.ofSeq
+
+let transitiveHullForNode graph node = 
+    Seq.singleton node |> transitiveHullForNodeSet graph
+
+let invert graph =
+    let inverseEdges = [for (a,b) in graph.edges -> (b,a)]
+    fromNodesAndEdges graph.nodes inverseEdges
---
+++ Untangle/Program.fs
@@ -0,0 +1,119 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+open System.Text.RegularExpressions
+
+let readGraphFromDotFile fileName = 
+    let dotParserGraph = IO.File.ReadAllText(fileName) |> DotParser.parse
+    let keysOfMap map = map |> Map.toSeq |> Seq.map fst |> Seq.toArray
+    let allNodes = dotParserGraph.Nodes |> keysOfMap
+    let allEdges = dotParserGraph.Edges |> keysOfMap
+    DependencyGraph.fromNodesAndEdges allNodes allEdges
+    
+let regexPredicate pattern = 
+    let regex = Regex(pattern)
+    fun s -> regex.IsMatch(s)
+
+module Untangle =
+    let nodesMatchingRegexes (graph : DependencyGraph.Graph) (regexes : seq<string>) =
+        let filterWithRegex nodes pattern = nodes |> Seq.filter (regexPredicate pattern)
+        regexes |> Seq.collect (filterWithRegex graph.nodes)
+
+    let hull (graph : DependencyGraph.Graph) (rootDefiningRegexes : seq<string>) = 
+        let rootNodes = nodesMatchingRegexes graph rootDefiningRegexes
+        DependencyGraph.transitiveHullForNodeSet graph rootNodes
+    
+    let inversehull (graph : DependencyGraph.Graph) (rootDefiningRegexes : seq<string>) =
+        hull (DependencyGraph.invert graph) rootDefiningRegexes
+    
+    let edgecheck (graph : DependencyGraph.Graph) (sourceRegex : string) (targetRegex : string) =
+        let sourcePredicate = regexPredicate sourceRegex
+        let targetPredicate = regexPredicate targetRegex
+        let predicate (s,t) = (sourcePredicate s) && (targetPredicate t)
+        graph.edges |> Seq.filter predicate
+    
+    let checkcsv (graph : DependencyGraph.Graph) (csvFileName : string) =
+        let csvString = System.IO.File.ReadAllText(csvFileName)
+        let csvGraph = DependencyGraph.csvToGraph csvString
+        graph.edges |> Seq.filter (fun e -> not <| Set.contains e csvGraph.edges)
+
+    let clusterByDependentNodeCoverage (graph : DependencyGraph.Graph) (rootDefiningRegexes : seq<string>) =
+        let rootNodes = nodesMatchingRegexes graph rootDefiningRegexes
+
+        let nodesGroupedByRootNodeDependencies = 
+            let hulls = rootNodes |> Seq.map (DependencyGraph.transitiveHullForNode graph >> Set.ofSeq) |> Array.ofSeq
+            let hullMembershipsForNode n = hulls |> Array.map (Set.contains n)
+            graph.nodes |> Seq.groupBy hullMembershipsForNode |> Seq.map snd
+    
+        let nodeToGroupLabelMap =
+            let mappingsForGroup group = 
+                let groupLabel = group |> String.concat "\n"
+                let keyValuePair node = (node, groupLabel)
+                group |> Seq.map keyValuePair
+            nodesGroupedByRootNodeDependencies |> Seq.collect mappingsForGroup |> Map.ofSeq
+
+        let lookup k = defaultArg (Map.tryFind k nodeToGroupLabelMap) "???"
+
+        DependencyGraph.mapNodesMergeEdges graph lookup
+
+module Commands =
+    let (|PrefixNoCase|_|) (p:string) (s:string) =
+        if s.ToLower().StartsWith(p.ToLower()) then
+            Some(s)
+        else
+            None
+
+    let edgeToArrow (s,t) = s + " -> " + t
+        
+    let run graph argList =
+        match argList with
+            | PrefixNoCase "hull" _ :: rest -> 
+                printfn "%s" (Untangle.hull graph rest |> String.concat "\n")
+                0
+            | PrefixNoCase "inversehull" _ :: rest -> 
+                printfn "%s" (Untangle.inversehull graph rest |> String.concat "\n")
+                0        
+            | PrefixNoCase "csvmatrix" _ :: fileName :: [] ->
+                System.IO.File.WriteAllText(fileName, DependencyGraph.graphToCsv graph)
+                0
+            | PrefixNoCase "edgecheck" _ :: sourceRegex :: targetRegex :: [] ->
+                let matches = Untangle.edgecheck graph sourceRegex targetRegex |> Array.ofSeq
+                Seq.map edgeToArrow matches |> String.concat "\n" |> printfn "%s" 
+                matches.Length
+            | PrefixNoCase "checkcsv" _ :: fileName :: [] ->
+                let disallowedEdges = Untangle.checkcsv graph fileName |> Array.ofSeq
+                Seq.map edgeToArrow disallowedEdges |> String.concat "\n" |> printfn "%s" 
+                disallowedEdges.Length
+            | PrefixNoCase "clusterbyhulls" _:: rootNodeRegex :: dotFileName :: [] ->
+                let reducedGraph = Untangle.clusterByDependentNodeCoverage graph [rootNodeRegex]
+                System.IO.File.WriteAllText(dotFileName, DependencyGraph.graphToDot reducedGraph)
+                0
+            | _ -> 
+                printfn "Could not parse command(s)."
+                1
+
+[<EntryPoint>]
+let main argv = 
+    let dotFileName = defaultArg (Array.tryItem 0 argv) @"D:\sw\graphs\mm.depends.dot"
+    let argList = if (argv.Length < 2) then [] else argv |> List.ofSeq |> List.tail
+
+    let fullGraph = readGraphFromDotFile dotFileName
+    let filteredGraph = 
+        fullGraph
+        |> DependencyGraph.filterNodes (not << regexPredicate ".*Test.*")
+        |> DependencyGraph.filterNodes (not << regexPredicate "sdk.*")
+        |> DependencyGraph.filterNodes (not << regexPredicate ".*Companion.*")
+    
+    Commands.run filteredGraph (argv |> List.ofSeq |> List.skip 1)
+
+    //let coreToCoreEdges = Untangle.edgematch filteredGraph "Core$" "Core$" |> List.ofSeq
+    //printfn "%A" coreToCoreEdges
+
+    //let reducedGraph = Untangle.clusterByDependentNodeCoverage filteredGraph ["Core$"]
+    //
+    ////System.IO.File.WriteAllText(@"D:\sw\graphs\mergedDepends.dot", DependencyGraph.graphToDot reducedGraph)
+    //
+    //System.IO.File.WriteAllText(@"D:\sw\graphs\fullMatrix.csv", DependencyGraph.graphToCsv fullGraph)
+    //
+    //Console.ReadLine() |> ignore
+    // return an integer exit code
